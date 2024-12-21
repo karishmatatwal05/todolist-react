e1d06022399ca8d64c295c9ed51b539033432c2e
Karishma Tatwal <karishmatatwal2002@gmail.com>
2024-12-21 16:45:22 +0530

    first commit

---
+++ src/MyComponent/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='bg-light text-dark my-3 p-5'> 
+        This is an About
+        <p></p>Welcome to the To-Do List App, a simple and intuitive task management application built using React.js. This app is designed to help you stay organized and productive by efficiently managing your daily tasks.
+      
+    </div>
+  )
+}
